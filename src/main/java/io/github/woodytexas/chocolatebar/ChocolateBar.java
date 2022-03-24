46b1b628ff36dedd133b18dcafa7400236d524fa
woodytexas <squrooted@gmail.com>
2022-03-24 14:07:34 -0700

    big update yay
    Signed-off-by: woodytexas <squrooted@gmail.com>

---
+++ src/main/java/io/github/woodytexas/chocolatebar/ChocolateBar.java
@@ -0,0 +1,27 @@
+package io.github.woodytexas.chocolatebar;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ChocolateBar implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("chocolatebar");
+
+    public static final Item QUILTY_WRAPPER = new Item(new FabricItemSettings().group(ItemGroup.MISC).rarity(Rarity.UNCOMMON));
+    public static final Item TOOLCHAIN_WRAPPER = new Item(new FabricItemSettings().group(ItemGroup.MISC).rarity(Rarity.UNCOMMON));
+    public static final ChocolateBarItem CHOCOLATE_BAR = new ChocolateBarItem(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(2).saturationModifier(0).build()));
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("chocolatebar", "chocolate_bar"), CHOCOLATE_BAR);
+        Registry.register(Registry.ITEM, new Identifier("chocolatebar", "quilty_wrapper"), QUILTY_WRAPPER);
+        Registry.register(Registry.ITEM, new Identifier("chocolatebar", "toolchain_wrapper"), TOOLCHAIN_WRAPPER);
+    }
+}
--- src/main/java/io/github/woodytexas/chocolatebar/ChocolateBarItem.java
+++ src/main/java/io/github/woodytexas/chocolatebar/ChocolateBarItem.java
@@ -1,13 +1,21 @@
 package io.github.woodytexas.chocolatebar;
 
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class ChocolateBarItem extends Item {
     public ChocolateBarItem(Settings settings) {
@@ -15,8 +23,9 @@ public class ChocolateBarItem extends Item {
     }
 
     @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-        playerEntity.playSound(SoundEvents.AMBIENT_UNDERWATER_ENTER, 1.0f, 1.0f);
-        return TypedActionResult.success(playerEntity.getStackInHand(hand));
+    public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext options) {
+        MutableText test = new LiteralText("Potato chocolate?").styled(style -> style.withColor(0xB79268));
+
+        tooltip.add(test);
     }
 }
